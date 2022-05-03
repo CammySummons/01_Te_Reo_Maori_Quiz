3e04721204b8bd13dd72ec53345da316dbb9cabc
CammySummons <64822645+CammySummons@users.noreply.github.com>
2022-05-04 10:56:32 +1200

    Updates

--- 01_Te_Reo_Maori_Quiz_v1.py
+++ 01_Te_Reo_Maori_Quiz_v1.py
@@ -17,33 +17,68 @@ opt2_text = StringVar()
 opt3_text = StringVar()
 opt4_text = StringVar()
 btn_disabled = False  # Buttons do nothing if btn_disabled = True
-num = 0
+num = 0  # Question number
+num_correct = 0  # Number of questions guest correctly
 current_question = StringVar()
 current_question.set(f"{num}/10")
 
 
-def option_selected():
+def option_selected(what_btn):
     global num
+    global num_correct
+    global btn_disabled
     if not btn_disabled:
-        if num < 10:
+        if num < 11:
             num += 1
 
-            # If correct...
+            # If answer == button text and not welcome screen:
+            if eval(f"qst_{num-1}.answer") == eval(f"qst_{num-1}.btn{what_btn}_text") and num != 1:
+                num_correct += 1
+                output_box.config(bg="lime")
+                output_box_txt.set("Correct!")
+            elif eval(f"qst_{num-1}.answer") != eval(f"qst_{num-1}.btn{what_btn}_text") and num != 1:
+                output_box.config(bg="red")
+                output_box_txt.set("Incorrect!")
+
+            if num == 11:
+                btn_disabled = True
 
             # Update buttons and labels
-            current_question.set(f"{num}/10")
-            opt1_text.set(eval(f"qst_{num}.btn1_text"))
-            opt2_text.set(eval(f"qst_{num}.btn2_text"))
-            opt3_text.set(eval(f"qst_{num}.btn3_text"))
-            opt4_text.set(eval(f"qst_{num}.btn4_text"))
-            output_box_txt.set(eval(f"qst_{num}.question"))
+            def up_btn():
+                if num <= 10:
+                    current_question.set(f"{num}/10")
+                else:
+                    qst_11 = Question(11, f"You finished the quiz and got {num_correct}/10 question"
+                                      f" correct.\nPress restart to play again.", "", "", "", "", "")
+                opt1_text.set(eval(f"qst_{num}.btn1_text"))
+                opt2_text.set(eval(f"qst_{num}.btn2_text"))
+                opt3_text.set(eval(f"qst_{num}.btn3_text"))
+                opt4_text.set(eval(f"qst_{num}.btn4_text"))
+                output_box_txt.set(eval(f"qst_{num}.question"))
+                output_box.config(bg="white")
+
+            # Time delay (1 sec) for button and label appearance
+            if num > 1:
+                output_box.after(1000, up_btn)
+            else:
+                up_btn()
 
 
-def opt_btn_framework(text):
-    return Button(frame, bg="light green", command=option_selected, textvariable=text, font=("Comic Sans MS", 14, "bold"))
+def opt_btn_framework(text, btn_id):
+    return Button(frame, bg="light green", command=lambda: option_selected(btn_id), textvariable=text, font=("Comic Sans MS", 14, "bold"))
 
 
-class Options:
+def restart():
+    global num
+    global num_correct
+    global btn_disabled
+    num = 0
+    num_correct = 0
+    btn_disabled = False
+    option_selected(1)
+
+
+class Question:
     def __init__(self, qst_num, question, btn1_text, btn2_text, btn3_text, btn4_text, answer):
         self.qst_num = qst_num
         self.question = question
@@ -55,19 +90,19 @@ class Options:
 
 
 # Questions
-qst_0 = Options(0, "Welcome to the Te Reo Maori Quiz! "
-                   "Press any of the buttons below to begin the quiz.",
-                   "Click", "One", "Of", "Us", "answer")  # qst_0 is the welcome screen
-qst_1 = Options(1, "What is 'red' in Maori ?", "Kahurangi", "Whero", "Rohi", "Rongo", "Whero")
-qst_2 = Options(2, "What colour is 'kākāriki' ?", "Yellow", "Blue", "Green", "Orange", "Green")
-qst_3 = Options(3, "What is 'orange' in Maori ?", "Karaka", "Kahurangi", "Kākāriki", "Ngako", "Karaka")
-qst_4 = Options(4, "What colour is 'papura' ?", "Red", "Violet", "Yellow", "Purple", "Purple")
-qst_5 = Options(5, "What is 'yellow' in Maori ?", "Whero whero", "Kōwhai", "Koura", "karekau", "Kōwhai")
-qst_6 = Options(6, "What colour is 'Karekau' ?", "Silver", "Black", "Turquoise", "Crimson", "Turquoise")
-qst_7 = Options(7, "What is 'lime' in Maori ?", "Pokarekare", "Ma", "Kotakota", "kākāriki", "Kotakota")
-qst_8 = Options(8, "What colour is 'pango' ?", "Black", "Red", "White", "Yellow", "Black")
-qst_9 = Options(9, "What is 'White' in Maori ?", "Kōwhai", "Ma", "Rohi", "Whero", "Ma")
-qst_10 = Options(10, "What colour is 'parauri' ?", "Gold", "Purple", "Pink", "Brown", "Brown")
+qst_0 = Question(0, "Welcome to the Te Reo Maori Quiz! "
+                    "Press any of the buttons below to begin the quiz.",
+                    "Click", "One", "Of", "Us", "answer")  # qst_0 is the welcome screen
+qst_1 = Question(1, "What is 'red' in Maori ?", "Kahurangi", "Whero", "Rohi", "Rongo", "Whero")
+qst_2 = Question(2, "What colour is 'kākāriki' ?", "Yellow", "Blue", "Green", "Orange", "Green")
+qst_3 = Question(3, "What is 'orange' in Maori ?", "Karaka", "Kahurangi", "Kākāriki", "Ngako", "Karaka")
+qst_4 = Question(4, "What colour is 'papura' ?", "Red", "Violet", "Yellow", "Purple", "Purple")
+qst_5 = Question(5, "What is 'yellow' in Maori ?", "Whero whero", "Kōwhai", "Koura", "karekau", "Kōwhai")
+qst_6 = Question(6, "What colour is 'Karekau' ?", "Silver", "Black", "Turquoise", "Crimson", "Turquoise")
+qst_7 = Question(7, "What is 'lime' in Maori ?", "Pokarekare", "Ma", "Kotakota", "kākāriki", "Kotakota")
+qst_8 = Question(8, "What colour is 'pango' ?", "Black", "Red", "White", "Yellow", "Black")
+qst_9 = Question(9, "What is 'White' in Maori ?", "Kōwhai", "Ma", "Rohi", "Whero", "Ma")
+qst_10 = Question(10, "What colour is 'parauri' ?", "Gold", "Purple", "Pink", "Brown", "Brown")
 
 
 # Declaring widgets
@@ -75,10 +110,11 @@ output_box = Label(frame, textvariable=output_box_txt,
                    font=("Comic Sans MS", 13), bg="white", fg="black")
 cur_que_border = Label(frame, bg="black")
 current_question_box = Label(frame, textvariable=current_question, font=("Comic Sans MS", 9, "bold"), bg="green", fg="black")
-option1 = opt_btn_framework(opt1_text)
-option2 = opt_btn_framework(opt2_text)
-option3 = opt_btn_framework(opt3_text)
-option4 = opt_btn_framework(opt4_text)
+option1 = opt_btn_framework(opt1_text, 1)
+option2 = opt_btn_framework(opt2_text, 2)
+option3 = opt_btn_framework(opt3_text, 3)
+option4 = opt_btn_framework(opt4_text, 4)
+restart_btn = Button(frame, bg="pink", command=restart, text="Restart", font=("Comic Sans MS", 12, "bold"))
 
 # Placing widgets
 output_box.place(x=150, y=100, width=725, height=80)
@@ -88,6 +124,7 @@ option1.place(x=150, y=250, width=320, height=80)
 option2.place(x=555, y=250, width=320, height=80)
 option3.place(x=150, y=400, width=320, height=80)
 option4.place(x=555, y=400, width=320, height=80)
+restart_btn.place(x=900, y=20, width=80, height=35)
 
 
 # Welcome screen
